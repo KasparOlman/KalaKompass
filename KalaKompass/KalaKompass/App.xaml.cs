c7b671d529b66d11109b50f8a7ca5c66fea7c8ff
Kaspar <kaspar.olman97gmail.com>
2023-12-08 11:57:52 +0200

    init

---
+++ KalaKompass/KalaKompass/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace KalaKompass;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ KalaKompass/KalaKompass/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace KalaKompass;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
