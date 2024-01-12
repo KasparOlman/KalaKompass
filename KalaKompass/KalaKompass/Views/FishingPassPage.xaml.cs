acba3b3867f9a0c151a87313904e3317740fc51d
madis <madis.pihlasalu@gmail.com>
2024-01-12 10:48:32 +0200

    main push

--- KalaKompass/KalaKompass/Views/FishingPassPage.xaml.cs
+++ KalaKompass/KalaKompass/Views/FishingPassPage.xaml.cs
@@ -6,4 +6,41 @@ public partial class FishingPassPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    async void FishingPass_Clicked(object sender, EventArgs e)
+    {
+        try
+        {
+            // Specify the external link
+            var uri = new Uri("https://kalaluba.ee/fishingCard");
+
+            // Open the default browser with the specified URI
+            await Launcher.OpenAsync(uri);
+        }
+        catch (Exception ex)
+        {
+            // Handle exceptions, e.g., if the URI is invalid
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+    }
+
+    async void HobbyPass_Clicked(object sender, EventArgs e)
+    {
+        try
+        {
+            // Specify the external link
+            var uri = new Uri("https://kalaluba.ee/hobbyFishingPermit");
+
+            // Open the default browser with the specified URI
+            await Launcher.OpenAsync(uri);
+        }
+        catch (Exception ex)
+        {
+            // Handle exceptions, e.g., if the URI is invalid
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+    }
+
+
 }
+
